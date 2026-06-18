cb560a29d01b41dbd52e464a711d5c93cae56f3a
lmoralesvega <lmoralesdelavega@gmail.com>
2026-06-18 15:19:03 -0600

    Remove Streamlit installation command
    Removed the Streamlit installation command from the script.

--- reto_dashbboard.py
+++ reto_dashbboard.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1Gn9nkk92Qb2SjM7tSo9GHnC-OOIWjrug
 """
 
-!pip install -q streamlit
-
 # Commented out IPython magic to ensure Python compatibility.
 # %%writefile app.py
 # import streamlit as st
